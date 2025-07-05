1c559969d11ae6686bdfadb990050e3d8c257f31
Kai Hinterholzer <khinterholzer9@gmail.com>
2025-07-05 15:05:06 +0200

    Setting up React

---
+++ project/client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Fleet Management Dashboard</h1>
+      <p>Frontend is up and running.</p>
+    </div>
+  );
+}
+
+export default App;
